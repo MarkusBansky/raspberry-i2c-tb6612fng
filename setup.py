2e539e620f76e77bb1efb7f45794af94236de8e0
MarkusBansky <MarkusBansky@users.noreply.github.com>
2020-06-02 22:53:47 +0100

    Modified project structure to fit module

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#
+#      Copyright (C) 2020  Markiian Benovskyi
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
@@ -5,23 +22,24 @@ with open("README.md", "r") as fh:
 
 setup(
     name='raspberry-i2c-tb6612fng',
-    version='0.1.0a1',
+    version='0.1.0a6',
     packages=find_packages(exclude=['tests*']),
-    license='Creative Commons Attribution-ShareAlike 4.0 International Public License',
+    license='GNU General Public License',
     description='A port of Grove Motor Driver TB6612FNG library for python and RaspberryPI. '
                 'This library also includes easing functions to control your motors with smooth transitions.'
                 'Easing functions are available for dc motors only.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['smbus', 'time', 'math'],
+    install_requires=['smbus'],
     keywords='raspberry i2c tb6612fng grove',
     url='https://github.com/MarkusBansky/raspberry-i2c-tb6612fng',
     author='Markiian Benovskyi',
     author_email='admin@markiian-benovskyi.com',
-    python_requires='>=2.7',
+    python_requires='>=3.6',
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "License :: OSI Approved :: Creative Commons License",
-        "Operating System :: Linux",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
     ],
 )
--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from i2c_tb6612fng_driver import MotorDriverTB6612FNG
-from i2c_tb6612fng_codes import TB6612FNGCodes
-from i2c_tb6612fng_motors import TB6612FNGMotors
-from i2c_tb6612fng_stepper import TB6612FNGStepper
-from i2c_tb6612fng_easings import tb6612fng_easy, tb6612fng_easy_fn_in_cubic, tb6612fng_easy_fn_in_expo, \
-    tb6612fng_easy_fn_in_quad, tb6612fng_easy_fn_out_cubic, tb6612fng_easy_fn_out_expo, \
-    tb6612fng_easy_fn_out_quad
